3a4c74c983a1aae57120d5358a3b99f2cdcc43b8
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-06-04 14:06:00 +0300

    testing runfile

--- run.rb
+++ run.rb
@@ -1,4 +1,31 @@
 require_relative 'config/environment'
 
+# user = User.first
+# puts user.reviews.inspect
+
 user = User.first
-puts user.reviews.inspect
+puts "User: #{user.name}"
+user1 = User.last
+puts "User: #{user1.name}"
+
+product = Product.find(4)
+puts "Product: #{product.name}"
+
+review = Review.find(4)
+puts "Review by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+
+user = User.first
+favourite_product = user.favourite_product
+
+if favourite_product
+  puts "Favorite product of #{user.name} is #{favourite_product.name}"
+else
+  puts "#{user.name} has no favorite product"
+end
+
+# puts "Review for #{product.name} by #{user.name}: #{star_rating}. #{comment}"
+review = Review.first
+
+puts "Review for #{review.product.name} by #{review.user.name}:"
+puts "Star rating: #{review.star_rating}"
+puts "Comment: #{review.comment}"
